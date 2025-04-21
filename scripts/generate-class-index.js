e69413235b0cc8135d6a695d6b31f20c9662e6f5
Jeido <tourais.q@gmail.com>
2025-04-21 12:44:35 +0200

    rogue div²

--- scripts/generate-class-index.js
+++ scripts/generate-class-index.js
@@ -89,7 +89,6 @@ const finalHtml = `<!DOCTYPE html>
     <header class="codex-header">
       <img src="../../assets/images/banniere.png" alt="Bannière" class="banniere">
         <h1><img src="../../assets/images/green-book.png" class="image codex-image-header"> Index des Classes</h1>
-      </div>
       <p class="sous-titre">Archives et mises à jour du monde d’Etrian Odyssey</p>
       <div class="fixed-header-links">
       <a href="/" class="carte-lien" style="display: inline-block; max-width: 300px;"><i class="fa-solid fa-arrow-left"></i> Retour au Codex</a>
